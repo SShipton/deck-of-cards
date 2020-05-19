c251054fd14fe0b085e4241971dff8e36eb8da3d
Skylar Shipton <skylarshipton@outlook.com>
2020-05-19 01:05:01 -0400

    Redone using the code from my Blackjack game.

--- Program.cs
+++ Program.cs
@@ -4,61 +4,50 @@ using System.Linq;
 
 namespace deck_of_cards
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Card
     {
-      // List of Suits
-      Random rnd = new Random();
-      var suits = new List<string>() {"Spades", "Clubs", "Diamonds", "Hearts",};
-      // List of Ranks
-      var ranks = new List<string>() {"Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"};
+        public string Face { get; set; }
+        public string Suit { get; set; }
 
-      int ranksIndex = rnd.Next(ranks.Count);
-      Console.Write("");
-      Console.Write(ranks[ranksIndex]);
-
-      int suitsIndex = rnd.Next(suits.Count);
-      Console.Write(" of ");
-      Console.Write(suits[suitsIndex]);
-
-      Console.Write(" and ");
-      int ranksIndexTwo = rnd.Next(ranks.Count);
-      Console.Write("");
-      Console.Write(ranks[ranksIndexTwo]);
-
-      int suitsIndexTwo = rnd.Next(suits.Count);
-      Console.Write(" of ");
-      Console.Write(suits[suitsIndexTwo]);
-
-      
-     // var n = 52
-     //  var n = deck.count ?
-      
-    
-
-
-
-
-      // make n = 52 since we are dealing with 52 elements
-      // var n = 52;
-      // for firstIndex from n - 1 down to 1 do:
-      //  secondIndex = random integer that is greater than or equal to 0 and LESS than firstIndex
-      //  Now swap the values at firstIndex and secondIndex by doing this:
-      //  firstValue = the value from items[firstIndex]
-      //  secondValue = the value from items[secondIndex]
-      //  items[firstIndex] = secondValue
-      //  items[secondIndex] = firstValue
- 
- 
- 
- 
- 
- 
- 
- 
- 
     }
-  }
+    class Program
+    {
+        private List<Card> Cards = new List<Card>();
+        public void MakeCards()
+        {
+
+            var suits = new string[] { "Clubs", "Diamonds", "Hearts", "Spades" };
+            var faces = new string[] { "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace" };
+
+            foreach (var suit in suits)
+            {
+                foreach (var face in faces)
+                {
+                    var newCard = new Card()
+                    {
+                        Suit = suit,
+                        Face = face,
+                    };
+
+                    Cards.Add(newCard);
+                }
+            }
+
+            var rNG = new Random();
+
+            for (var index = Cards.Count - 1; index >= 1; index--)
+            {
+                var otherIndex = rNG.Next(index);
+
+                var firstCard = Cards[index];
+                var otherCard = Cards[otherIndex];
+
+                Cards[index] = otherCard;
+                Cards[otherIndex] = firstCard;
+            }
+        }
+    }
 }
+
+
 
